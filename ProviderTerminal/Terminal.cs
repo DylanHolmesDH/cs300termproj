c37b929542a7e93b537f17a92b610318764c3e2c
ProceduralCode <37784785+ProceduralCode@users.noreply.github.com>
2021-03-11 18:19:28 -0800

    Complete Provider Terminal Overhaul
    Sorry Felina :/
    Plz don't hate me

--- ProviderTerminal/Terminal.cs
+++ ProviderTerminal/Terminal.cs
@@ -5,136 +5,138 @@ using ChocAnDatabase;
 using ChocAnDatabase.records;
 
 namespace ProviderTerminal {
-
-    //possible options. includes success and failure for verfication purposes
-    enum option
-    {
-        member = 1,
-        providerDir = 2,
-        consultRec = 3,
-        veriBill = 4,
-        quit = 5,
-        success = 6,
-        fail = 7
-    }
-
     class Terminal {
-        private int providerId, activeMember;
-        private Database db;
-        private String providerName;
-
+        Database db;
+        ProviderRecord provider;
+        MemberRecord member;
+        List<String> exitWords = new List<String>{ "e", "E", "exit", "Exit", "q", "Q", "quit", "Quit", };
+        
         static void Main(String[] args) {
             Terminal term = new Terminal();
             term.Start();
         }
 
-        //Start of the Provider Terminal
         public void Start() {
-            option flag;
-            option login_success;
             db = new Database();
+            Dictionary<string, object> temp = new Dictionary<string, object>();
+            provider = new ProviderRecord(temp);
+            provider.Name = "Emerson";
+            provider.Number = 69;
+            provider.State = "OR";
+            provider.Zip = 97531;
+            provider.Address = "SrawberryLn";
+            provider.City = "Portland";
+            db.InsertProvider(provider);
+            db.Save();
+            return;
 
-            //repeats until user either quits or gets the right login
-            do {
-                login_success = Login();
-            } while (login_success == option.fail);
-
-            //only enters loop if the login was successful
-            do {
-                PrintOptions();
-                flag = GetInputOption("Please enter a number: ");
-                switch (flag) {
-                    //checks member number, goes to member sub menu
-                    case option.member:
-                        VerifyAndSetActiveMember();
-                        break;
-                    //generates entire provider directory in alphabetical order
-                    case option.providerDir:
-                        GenerateProviderDirectory();
-                        break;
-                    //for provider to create a record by filling out a form
-                    case option.consultRec:
-                        CreateConsulationRecord();
-                        break;
-                    //checks if record exists in database, and if it does, displays it
-                    case option.veriBill:
-                        VerificationOfBilling();
-                        break;
-                    //to exit program
-                    case option.quit:
-                        break;
-                    //otherwise input is bad
-                    default:
-                        Console.WriteLine("Please select a valid option!");
-                        Console.ReadLine();
-                        break;
+            // Login
+            Console.Clear();
+            Console.WriteLine("LOGIN");
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine("Please enter your ID number. To exit this program, type 'quit' instead.\n\n");
+            while (true) {
+                String input = GetInputString("ID Number: ");
+                if (input == "" || exitWords.Contains(input)) {
+                    return;
                 }
-            } while (flag != option.quit);
+				try {
+                    provider = db.FetchProvider(Convert.ToInt32(input));
+				} catch (FormatException) {
+					Console.WriteLine("Please enter a valid number (or 'quit')");
+                    continue;
+				}
+				if (provider == null) {
+                    Console.WriteLine("ID number does not exist. Please try again.");
+                    continue;
+                }
+                break;
+            }
 
+            // Main menu
+            bool exit = false;
+			while (!exit) {
+                Console.Clear();
+                Console.WriteLine("---------------------------------------------------");
+                Console.WriteLine("Welcome, " + provider.Name + ".\n");
+                Console.WriteLine("\t[1] Check-in Member");              // Checks member number, goes to member sub menu
+                Console.WriteLine("\t[2] Check the Provider Directory"); // Generates entire provider directory in alphabetical order
+                Console.WriteLine("\t[3] Create Consultation Record");   // For provider to create a record by filling out a form
+                Console.WriteLine("\t[4] Verify a Bill");                // Checks if record exists in database, and if it does, displays it
+                Console.WriteLine("\t[0] Exit\n");
+                while (true) {
+                    String input = GetInputString("Please enter an option: ");
+                    if (input == "1") {
+                        CheckinMember();
+                    } else if (input == "2") {
+                        GenerateProviderDirectory();
+                    } else if (input == "3") {
+                        CreateConsulationRecord();
+                    } else if (input == "4") {
+                        VerificationOfBilling();
+                    } else if (input == "0" || input == "" || exitWords.Contains(input)) {
+                        exit = true;
+                        Console.WriteLine("See ya!");
+                    } else {
+                        Console.WriteLine("Sorry, that's not a valid option.");
+                        continue;
+                    }
+                    break;
+                }
+            }
             db.Save();
         }
 
-        private option Login() {
-            String input = null;
+        // Checks if a member number is valid, if their membership is suspended, or if they were never a member
+        void CheckinMember() {
+            Console.Clear();
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine(provider.Name + "\n\n\n\n\n\n\n");
 
-            try {
-                //clear the screen of text and display a login "menu"
-                //get the provider number
-                Console.Clear();
-                Console.WriteLine("LOGIN");
-                Console.WriteLine("---------------------------------------------------");
-                Console.WriteLine("Please enter your ID number. To exit this program, type 'quit' instead.\n\n");
-                input = GetInputString("ID Number: ");
-                providerId = Convert.ToInt32(input);
-
-                if (input == "quit" || input == "'quit'" || input == "q")
-                    return option.quit;
-
-                var record = db.FetchProvider(providerId);
-
-                //check if the provider number exists in the database 
-                if (record == null) {
-                    Console.WriteLine("ID number does not exist. Please try again.");
-                    return option.fail;
+            // Get a valid member from user
+            while (true) {
+                String input = GetInputString("Please enter the member id (swipe card): ");
+                if (input == "" || exitWords.Contains(input)) {
+                    return;
                 }
-                //provider num exists in database
-                providerName = record.Name;
-                return option.success;
-            } catch (Exception e) {
-                Console.WriteLine(e.Message);
-                return option.fail;
+                try {
+                    member = db.FetchMember(Convert.ToInt32(input));
+                } catch (FormatException) {
+                    Console.WriteLine("Please enter a valid number (or 'quit')");
+                    continue;
+                }
+                if (member == null) {
+                    Console.WriteLine("Sorry, that's not an existing member.");
+                    continue;
+                }
+                if (!member.Validated) {
+                    Console.WriteLine("Sorry, member '" + member.Name + "' is not a valid member (anymore).");
+                    return;
+                }
+                Console.WriteLine("Member '" + member.Name + "' is now checked in.");
+                break;
             }
-        }
 
-        void PrintOptions() {
-            Console.Clear();
+            // Member menu
             Console.WriteLine("---------------------------------------------------");
-            Console.WriteLine(providerName + "\n");
-            Console.WriteLine("\t[" + (int)option.member + "] Verify a Member");
-            Console.WriteLine("\t[" + (int)option.providerDir + "] Check the Provider Directory");
-            Console.WriteLine("\t[" + (int)option.consultRec + "] Create Consultation Record");
-            Console.WriteLine("\t[" + (int)option.veriBill + "] Verify a Bill");
-            Console.WriteLine("\t[" + (int)option.quit + "] Exit\n");
-		}
+            Console.WriteLine(provider.Name + "; now serving " + member.Name + ".\n");
+            Console.WriteLine("\t[1] ");
+            Console.WriteLine("\t[0] Main menu\n");
+            while (true) {
+                String option = GetInputString("Please enter an option: ");
+                if (option == "1") {
 
-        //checks if a member number is valid, if their membership is suspended, or if they were never a member
-        bool VerifyAndSetActiveMember() {
-            Console.Clear();
-            Console.WriteLine("---------------------------------------------------");
-            Console.WriteLine(providerName + "\n\n\n\n\n\n\n");
-            activeMember = GetInputInt("Please enter the member id: ");
+                } else if (option == "2") {
 
-            //EDIT THIS
-            //we need to check is member is valid, suspended, doesnt exist in database
-            bool memberIsValid = true;
-            if (memberIsValid) {
-                Console.WriteLine("Member '" + activeMember + "' is valid.");
-                Console.ReadLine();
-            } else {
-                Console.WriteLine("Member '" + activeMember + "' is not valid.");
-                Console.ReadLine();
+                } else if (option == "0" || option == "" || exitWords.Contains(option)) {
+                    Console.WriteLine("Going back to main menu.");
+                    return;
+                } else {
+                    Console.WriteLine("Sorry, that's not a valid option.");
+                    continue;
+                }
+                break;
             }
-            return memberIsValid;
         }
 
         void GenerateProviderDirectory() {
@@ -145,17 +147,17 @@ namespace ProviderTerminal {
         }
 
         void CreateConsulationRecord() {
-            if (!VerifyAndSetActiveMember()) {
-                return;
-            }
+            //if (!CheckinMember()) {
+            //    return;
+            //}
             Dictionary<String, object> newRecordData = new Dictionary<String, object>();
             newRecordData.Add("record_date", GetInputDate("Record date: "));
             newRecordData.Add("service_date", GetInputDate("Service date: "));
-            while (!VerifyAndSetActiveMember()) {
-                Console.WriteLine("Please try again.");
-            }
-            newRecordData.Add("member_number", activeMember);
-            newRecordData.Add("provider_number", providerId);
+            //while (!CheckinMember()) {
+            //    Console.WriteLine("Please try again.");
+            //}
+            newRecordData.Add("member_number", member);
+            newRecordData.Add("provider_number", provider.Number);
             GenerateProviderDirectory();
             newRecordData.Add("service_number", GetInputInt("Service code: "));
             newRecordData.Add("comments", GetInputString("Any extra comments? "));
@@ -168,7 +170,7 @@ namespace ProviderTerminal {
             if (db.FetchConsultation(
                     GetInputInt("Member ID: "),
                     GetInputInt("Service code: "),
-                    providerId,
+                    provider.Number,
                     GetInputDate("Record date: "),
                     GetInputDate("Service date: ")) != null) {
                 Console.WriteLine("The info entered pertains to an existing record in the database.");
@@ -189,10 +191,6 @@ namespace ProviderTerminal {
         DateTime GetInputDate(String message = "") {
             Console.WriteLine(message);
             return DateTime.Parse(Console.ReadLine());
-        }
-
-        option GetInputOption(String message = "") {
-            return (option)GetInputInt(message);
         }
 
         List<String> GetInputMultiLine(String message = "") {
